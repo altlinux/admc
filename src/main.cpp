a2edfe1d5ad8e49e95249aa5320b852965075bf4
Dmitry Degtyarev <kevl@altlinux.org>
2020-05-25 13:54:47 +0400

    move entry actions out of context menu
    so that they can be shortcutted and put in menubar
    add get_selected_dn()

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,86 @@
 #include "attributes_model.h"
 #include "create_entry_dialog.h"
 #include "ad_interface.h"
-#include "entry_context_menu.h"
 #include "ui_mainwindow.h"
 
 #include <QApplication>
 #include <QString>
+#include <QAction>
+
+Ui::MainWindow ui;
+AttributesList *attributes_list;
+QAction *action_attributes;
+QAction *action_delete_entry;
+QList<QAction *> new_entry_actions;
+
+QString get_selected_dn() {
+    QTreeView *focus_view = nullptr;
+    
+    if (ui.containers_view->hasFocus()) {
+        focus_view = ui.containers_view;
+    } else if (ui.contents_view->hasFocus()) {
+        focus_view = ui.contents_view;
+    }
+
+    QString dn = "";
+
+    if (focus_view != nullptr) {
+        auto selection_model = focus_view->selectionModel();
+        auto selected_indexes = selection_model->selectedIndexes();
+
+        if (selected_indexes.size() > 0) {
+            auto selected = selected_indexes[0];
+            QModelIndex dn_index = selected.siblingAtColumn(AdModel::Column::DN);
+
+            dn = dn_index.data().toString();
+        }
+    }
+
+    return dn;
+}
+
+void on_action_attributes() {
+    auto dn = get_selected_dn();
+    attributes_list->set_target_dn(dn);
+}
+
+void on_action_delete_entry() {
+    auto dn = get_selected_dn();
+    delete_entry(dn);
+}
+
+void on_action_new_entry(NewEntryType type) {
+    QString dn = get_selected_dn();
+    create_entry_dialog(type, dn);
+}
+
+void popup_entry_context_menu(const QPoint &pos) {
+    QMenu menu;
+
+    menu.addAction(action_attributes);
+    menu.addAction(action_delete_entry);
+
+    QMenu *submenu_new = menu.addMenu("New");
+    for (auto action : new_entry_actions) {
+        submenu_new->addAction(action);
+    }
+
+    menu.exec(pos, action_attributes);
+}
+
+void connect_view_to_entry_context_menu(const QTreeView &view) {
+    QObject::connect(
+        &view, &QWidget::customContextMenuRequested,
+        [&view] (const QPoint &pos) {
+            // Get DN of clicked entry
+            QModelIndex index = view.indexAt(pos);
+
+            if (index.isValid()) {
+                QPoint global_pos = view.mapToGlobal(pos);
+                popup_entry_context_menu(global_pos);
+            }
+        });
+}
 
 int main(int argc, char **argv) {
     // Load fake AD data if given "fake" argument
@@ -37,23 +112,33 @@ int main(int argc, char **argv) {
 
     ContainersTree containers_tree(ui.containers_view, &ad_model, ui.menubar_view_advancedView);
     ContentsList contents_list(ui.contents_view, &ad_model, ui.menubar_view_advancedView);
-    AttributesList attributes_list(ui.attributes_view);
+    attributes_list = new AttributesList(ui.attributes_view);
 
-    // Entry context menu
-    auto entry_context_menu = new EntryContextMenu();
-    entry_context_menu->connect_view(*(ui.containers_view));
-    entry_context_menu->connect_view(*(ui.contents_view));
-
-    // Set target dn of attributes view when attributes menu of
-    // entry context menu is clicked
+    // Setup actions
+    action_attributes = new QAction("Attributes");
+    action_delete_entry = new QAction("Delete");
     QObject::connect(
-        entry_context_menu, &EntryContextMenu::attributes_clicked,
-        &attributes_list, &AttributesList::set_target_dn);
-
-    // Delete entry when delete button is pressed
+        action_attributes, &QAction::triggered,
+        on_action_attributes);
     QObject::connect(
-        entry_context_menu, &EntryContextMenu::delete_clicked,
-        delete_entry);
+        action_delete_entry, &QAction::triggered,
+        on_action_delete_entry);
+
+    // Setup "New X" actions
+    for (int type_i = NewEntryType::User; type_i < NewEntryType::COUNT; type_i++) {
+        NewEntryType type = static_cast<NewEntryType>(type_i);
+        QString label = new_entry_type_to_string[type];
+        QAction *action = new QAction(label);
+
+        QObject::connect(action, &QAction::triggered, [type]() {
+            create_entry_dialog(type);
+        });
+        
+        new_entry_actions.push_back(action);
+    }
+
+    connect_view_to_entry_context_menu(*ui.containers_view);
+    connect_view_to_entry_context_menu(*ui.contents_view);
 
     // Set root index of contents view to selection of containers view
     QObject::connect(
