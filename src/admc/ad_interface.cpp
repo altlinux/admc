372a60d6e2557d099fa641632ccd6bf2788fd411
Dmitry Degtyarev <kevl@altlinux.org>
2020-12-23 13:58:49 +0400

    fix adding users to groups by drag and drop

--- src/admc/ad_interface.cpp
+++ src/admc/ad_interface.cpp
@@ -889,7 +889,7 @@ DropType get_drop_type(const QString &dn, const QString &target_dn) {
     const AdObject target = AD()->search_object(target_dn, {ATTRIBUTE_OBJECT_CLASS});
 
     const bool dropped_is_user = dropped.is_class(CLASS_USER);
-    const bool target_is_group = target.is_class(CLASS_USER);
+    const bool target_is_group = target.is_class(CLASS_GROUP);
 
     if (dropped_is_user && target_is_group) {
         return DropType_AddToGroup;
