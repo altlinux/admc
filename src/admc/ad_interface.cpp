ee31c461e69a53c97ba3fc2e7e59ddfcea8dc8ab
Dmitry Degtyarev <kevl@altlinux.org>
2021-02-01 16:33:02 +0400

    allow drag and drop group on group
    for adding group to another group as member

--- src/admc/ad_interface.cpp
+++ src/admc/ad_interface.cpp
@@ -946,9 +946,13 @@ DropType get_drop_type(const QString &dn, const QString &target_dn) {
     const AdObject target = AD()->search_object(target_dn, {ATTRIBUTE_OBJECT_CLASS});
 
     const bool dropped_is_user = dropped.is_class(CLASS_USER);
+    const bool dropped_is_group = dropped.is_class(CLASS_GROUP);
     const bool target_is_group = target.is_class(CLASS_GROUP);
 
+
     if (dropped_is_user && target_is_group) {
+        return DropType_AddToGroup;
+    } else if (dropped_is_group && target_is_group) {
         return DropType_AddToGroup;
     } else {
         const bool system_flags_forbid_move = dropped.get_system_flag(SystemFlagsBit_CannotMove);
