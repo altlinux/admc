dc56a901f1a0a24ba83048f085ead9819064da0e
Dmitry Degtyarev <kevl@altlinux.org>
2021-02-20 11:49:09 +0400

    only disable apply/reset buttons on success
    if any errors happen, buttons stay enabled

--- src/admc/properties_dialog.cpp
+++ src/admc/properties_dialog.cpp
@@ -224,10 +224,12 @@ bool PropertiesDialog::apply() {
         tab->apply(target);
     }
 
-    apply_button->setEnabled(false);
-    reset_button->setEnabled(false);
-
     const bool success = STATUS()->end_error_log(this);
+
+    if (success) {
+        apply_button->setEnabled(false);
+        reset_button->setEnabled(false);
+    }
 
     hide_busy_indicator();
 
