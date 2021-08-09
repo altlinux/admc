19bcdc001d23c304899ff2469f6e3aa89baee6aa
Dmitry Degtyarev <kevl@altlinux.org>
2021-08-09 15:38:38 +0400

    define dummy %packager

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Need to add this so that the %packager macro used in .spec
+# is defined, actual value doesn't matter since this is just
+# for testing
+echo "%packager Docker <docker@email.com>" >> ~/.rpmmacros
+
 cd /app/ && gear-rpm -ba
