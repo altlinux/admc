6d70920ef53cf52bff76bd0b59d03d4f55b1f666
Dmitry Degtyarev <kevl@altlinux.org>
2021-08-30 16:34:08 +0400

    add item_type.h
    forgot to add file to git

---
+++ src/admc/item_type.h
@@ -0,0 +1,35 @@
+/*
+ * ADMC - AD Management Center
+ *
+ * Copyright (C) 2020-2021 BaseALT Ltd.
+ * Copyright (C) 2020-2021 Dmitry Degtyarev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ITEM_TYPE_H
+#define ITEM_TYPE_H
+
+enum ItemType {
+    ItemType_Unassigned,
+    ItemType_Object,
+    ItemType_PolicyRoot,
+    ItemType_Policy,
+    ItemType_QueryFolder,
+    ItemType_QueryItem,
+
+    ItemType_LAST,
+};
+
+#endif /* ITEM_TYPE_H */
