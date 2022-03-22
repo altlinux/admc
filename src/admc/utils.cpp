572809ed2f43f533bd3c86cb0b98a2c999765be5
Dmitry Degtyarev <kevl@altlinux.org>
2022-03-22 15:13:12 +0400

    check for leading question marks in object names
    prohibit them

--- src/admc/utils.cpp
+++ src/admc/utils.cpp
@@ -388,14 +388,15 @@ bool verify_object_name(const QString &name, QWidget *parent) {
         const bool some_bad_chars = string_contains_bad_chars(name, NAME_BAD_CHARS);
         const bool starts_with_space = name.startsWith(" ");
         const bool ends_with_space = name.endsWith(" ");
+        const bool starts_with_question_mark = name.startsWith("?");
 
-        const bool out = (some_bad_chars || starts_with_space || ends_with_space);
+        const bool out = (some_bad_chars || starts_with_space || ends_with_space || starts_with_question_mark);
 
         return out;
     }();
 
     if (contains_bad_chars) {
-        const QString error_text = QString(QCoreApplication::translate("utils.cpp", "Input field for Name contains one or more of the following illegal characters: # , + \" \\ < > ; = (leading space) (trailing space)"));
+        const QString error_text = QString(QCoreApplication::translate("utils.cpp", "Input field for Name contains one or more of the following illegal characters: # , + \" \\ < > ; = (leading space) (trailing space) (leading question mark)"));
         message_box_warning(parent, QCoreApplication::translate("utils.cpp", "Error"), error_text);
 
         return false;
