2386f3238460d2a942ba9187c616c32c6b4c03f0
Dmitry Degtyarev <kevl@altlinux.org>
2022-06-14 11:10:45 +0400

    remove github workflow
    because it's broken

--- script/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Need to add this so that the %packager macro used in .spec
-# is defined, actual value doesn't matter since this is just
-# for testing
-echo "%packager Docker <docker@email.com>" >> ~/.rpmmacros
-
-cd /app/ && gear-rpm -ba
