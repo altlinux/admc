4a63080773ed7a40b2d1c3c14690a7ee3d821077
Dmitry Degtyarev <kevl@altlinux.org>
2022-06-23 16:40:22 +0400

    fix filter combo's

--- src/admc/find_policy_dialog.cpp
+++ src/admc/find_policy_dialog.cpp
@@ -21,7 +21,16 @@
 #include "find_policy_dialog.h"
 #include "ui_find_policy_dialog.h"
 
+#include "adldap.h"
 #include "settings.h"
+
+enum SearchItem {
+    SearchItem_Name,
+    SearchItem_GUID,
+};
+
+// TODO: "not contains" item for condition combo. Need to
+// add Condition_NotContains to ad_filter.
 
 FindPolicyDialog::FindPolicyDialog(QWidget *parent)
 : QDialog(parent) {
@@ -29,6 +38,39 @@ FindPolicyDialog::FindPolicyDialog(QWidget *parent)
     ui->setupUi(this);
 
     setAttribute(Qt::WA_DeleteOnClose);
+
+    // Fill search item combo
+    const QList<SearchItem> search_item_list = {
+        SearchItem_Name,
+        SearchItem_GUID,
+    };
+
+    for (const SearchItem &search_item : search_item_list) {
+        const QString search_item_string = [&]() {
+            switch (search_item) {
+                case SearchItem_Name: return tr("Name");
+                case SearchItem_GUID: return tr("GUID");
+            }
+
+            return QString();
+        }();
+
+        ui->search_item_combo->addItem(search_item_string, (int) search_item);
+    }
+
+    // Fill condition combo
+    const QList<Condition> condition_list = {
+        Condition_Contains,
+        Condition_Equals,
+        Condition_StartsWith,
+        Condition_EndsWith,
+    };
+
+    for (const Condition &condition : condition_list) {
+        const QString condition_string = condition_to_display_string(condition);
+
+        ui->condition_combo->addItem(condition_string, (int) condition);
+    }
 
     settings_setup_dialog_geometry(SETTING_find_policy_dialog_geometry, this);
 }
