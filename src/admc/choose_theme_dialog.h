ac2eef8416f9f6c084eba398b6820e8d8ac6279b
Semyon Knyazev <knyazevsr@basealt.ru>
2022-12-06 18:18:56 +0400

    Theme selection dialog is refactored
    ChooseThemeDialog is merged with SelectThemeImpl class and refactored.
    SelectThemeImpl class is removed.

--- src/admc/choose_theme_dialog.h
+++ src/admc/choose_theme_dialog.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QStringListModel>
+#include <QDebug>
 namespace Ui {
     class ChooseThemeDialog;
 }
@@ -10,17 +11,28 @@ namespace Ui {
 class ChooseThemeDialog : public QDialog
 {
     Q_OBJECT
+
 public:
     ChooseThemeDialog(QWidget * parent);
     Ui::ChooseThemeDialog *ui;
-    void setup_theme_list();
-    void apply_theme();
 
 private slots:
-    void accepted();
+    void accept() override;
 
 private:
     QStringListModel * theme_model;
+    const QString app_theme_path = ":/icons";
+    const QString system_icons_dir_path = "/usr/share/icons";
+    QString user_icons_dir_path;
+
+    const QString theme_name_regex_pattern = "Name=.*";
+    const QString theme_file_name = "index.theme";
+
+    void setup_theme_list();
+    void apply_theme(QString theme_name);
+    QStringList get_available_theme_list();
+    QStringList parse_theme_names(const QStringList & theme_path_list);
+    QStringList get_themes_from_dir(QString path_to_dir);
 };
 
 #endif // CHOOSETHEMEDIALOG_H
