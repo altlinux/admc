ba27fd07f084179e966259caa87f96ce09b04148
Evgeny Sinelnikov <sin@altlinux.org>
2022-12-13 08:02:14 +0400

    Revert "Fixes on incorrect word wrapping"
    This reverts commit 0876c1a91499c4efea776edc5797ef54cd1e90e3.

--- src/admc/console_widget/console_widget_p.h
+++ src/admc/console_widget/console_widget_p.h
@@ -30,7 +30,6 @@
 
 #include <QCoreApplication>
 #include <QSet>
-#include <QTextEdit>
 
 class QLabel;
 class QStackedWidget;
@@ -66,8 +65,8 @@ public:
     ConsoleDragModel *model;
     ScopeProxyModel *scope_proxy_model;
     QWidget *description_bar;
-    QTextEdit *description_bar_left;
-    QTextEdit *description_bar_right;
+    QLabel *description_bar_left;
+    QLabel *description_bar_right;
     QAbstractItemView *focused_view;
     QStackedWidget *results_stacked_widget;
     QSplitter *splitter;
