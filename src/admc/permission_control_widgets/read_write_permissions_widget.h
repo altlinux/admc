50691758b30fc4d3119c787288eef486e09a7bb8
Semyon Knyazev <samael@altlinux.org>
2024-11-02 01:39:09 +0400

    Create read/write permissions widget
    Widget contains read and write permissions for
    attributes (properties) of the selected class.

---
+++ src/admc/permission_control_widgets/read_write_permissions_widget.h
@@ -0,0 +1,22 @@
+#ifndef READWRITEPERMISSIONSWIDGET_H
+#define READWRITEPERMISSIONSWIDGET_H
+
+#include "permissions_widget.h"
+
+
+class ReadWritePermissionsWidget final : public PermissionsWidget {
+    Q_OBJECT
+
+public:
+    explicit ReadWritePermissionsWidget(QWidget *parent);
+    ~ReadWritePermissionsWidget();
+
+    virtual void init(const QStringList &target_classes, security_descriptor *sd_arg) override;
+
+private:
+    virtual QList<QStandardItem*> create_item_row(const SecurityRight &right) override;
+    virtual bool right_applies_to_class(const SecurityRight &right, const QString &obj_class) override;
+    virtual bool there_are_rights_for_class(const QString &obj_class) override;
+};
+
+#endif // READWRITEPERMISSIONSWIDGET_H
