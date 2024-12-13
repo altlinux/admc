252a68de217561f93ac3b81edd2006f416c5bc81
Semyon Knyazev <samael@altlinux.org>
2024-12-13 19:11:23 +0400

    Add middle name showing setting-action to main window

--- src/admc/main_window.cpp
+++ src/admc/main_window.cpp
@@ -306,6 +306,7 @@ MainWindow::MainWindow(AdInterface &ad, QWidget *parent)
         {SETTING_show_non_containers_in_console_tree, ui->action_show_noncontainers},
         {SETTING_advanced_features, ui->action_advanced_features},
         {SETTING_load_optional_attribute_values, ui->action_load_optional_values},
+        {SETTING_show_middle_name_when_creating, ui->action_show_middle_name},
     };
 
     const QList<QString> simple_setting_list = {
@@ -314,7 +315,8 @@ MainWindow::MainWindow(AdInterface &ad, QWidget *parent)
         SETTING_log_searches,
         SETTING_timestamp_log,
         SETTING_show_login,
-        SETTING_load_optional_attribute_values
+        SETTING_load_optional_attribute_values,
+        SETTING_show_middle_name_when_creating
     };
 
     for (const QString &setting : bool_action_map.keys()) {
