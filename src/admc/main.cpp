altlinux/admc

commit 6efd30096f69f3b94a95cf6ef9e98dec7607f3b1

Return connection error dialog for failed connection on startup
Dialog is shown with actual credentials and failed connection on
start

src/admc/main.cpp
@@ -21,7 +21,6 @@
 
 #include "adldap.h"
 #include "config.h"
-#include "connection_options_dialog.h"
 #include "globals.h"
 #include "main_window.h"
 #include "main_window_connection_error.h"
@@ -113,6 +112,7 @@ int main(int argc, char **argv) {
     load_connection_options();
 
     MainWindow *main_window = nullptr;
+    MainWindowConnectionError *error_window = nullptr;
     {
         const bool show_login_window = settings_get_variant(SETTING_show_login_window_on_startup).toBool();
         if (show_login_window) {
@@ -127,7 +127,12 @@ int main(int argc, char **argv) {
             main_window->show_changelog_on_update();
         }
         else {
-            main_window->open_auth_dialog();
+            if (krb5_client->active_tgt_principals().isEmpty()) {
+                main_window->open_auth_dialog();
+            } else {
+                error_window = new MainWindowConnectionError(main_window);
+                error_window->show();
+            }
         }
     }
 

src/admc/CMakeLists.txt
@@ -68,6 +68,7 @@ set(ADMC_SOURCES
     tab_widget.cpp
     create_object_helper.cpp
     rename_object_helper.cpp
+    main_window_connection_error.cpp
 
     results_widgets/policy_ou_results_widget/policy_ou_results_widget.cpp
     results_widgets/policy_ou_results_widget/inherited_policies_widget.cpp

src/admc/connection_options_dialog.cpp
@@ -186,7 +186,8 @@ void ConnectionOptionsDialog::accept() {
 
     emit host_changed(selected_host);
 
-    if (!current_dc_is_master_for_role(ad, FSMORole_PDCEmulation)) {
+    if (!current_dc_is_master_for_role(ad, FSMORole_PDCEmulation) &&
+            !current_master_for_role_dn(ad, dn_from_role(FSMORole_PDCEmulation)).isEmpty()) {
         if (gpo_edit_without_PDC_disabled)
             g_status->add_message(tr("You are connected to DC without PDC-Emulator role. "
                                      "Group policy editing is prohibited by the setting."), StatusType_Success);

src/admc/main_window.cpp
@@ -80,11 +80,10 @@ MainWindow::MainWindow(AdInterface &ad, Krb5Client &krb5_client_arg, QWidget *pa
         init_on_connect(ad);
     }
     else {
-        if (!krb5_client->current_principal().isEmpty()) {
-            krb5_client->logout(false);
-        }
         g_status->log_messages(ad.messages());
-        login_label->setText(tr("Authentication required"));
+        if (krb5_client->current_principal().isEmpty()) {
+            login_label->setText(tr("Authentication required"));
+        }
     }
 }
 

src/admc/main_window.h
@@ -82,6 +82,8 @@ private:
     void disable_actions_on_logout(bool disable);
     void init_krb5_client();
     void resize_status_message();
+
+    friend class MainWindowConnectionError;
 };
 
 #endif /* MAIN_WINDOW_H */

src/admc/main_window_connection_error.cpp
@@ -28,10 +28,12 @@
 #include "settings.h"
 #include "utils.h"
 
-MainWindowConnectionError::MainWindowConnectionError()
-: QMainWindow() {
+MainWindowConnectionError::MainWindowConnectionError(MainWindow *main_window_arg)
+: QDialog(main_window_arg), main_window(main_window_arg) {
     ui = new Ui::MainWindowConnectionError();
     ui->setupUi(this);
+
+    setAttribute(Qt::WA_DeleteOnClose);
 
     center_widget(this);
 
@@ -54,12 +56,10 @@ void MainWindowConnectionError::on_retry_button() {
     AdInterface ad;
 
     if (ad_connected(ad, this)) {
-        load_g_adconfig(ad);
+        main_window->init_on_connect(ad);
+        main_window->show();
 
-        MainWindow *real_main_window = new MainWindow(ad, this);
-        real_main_window->show();
-
-        QMainWindow::hide();
+        QDialog::close();
     }
 }
 

src/admc/main_window_connection_error.h
@@ -21,24 +21,28 @@
 #ifndef MAIN_WINDOW_PRE_H
 #define MAIN_WINDOW_PRE_H
 
-#include <QMainWindow>
+#include <QDialog>
 
 namespace Ui {
 class MainWindowConnectionError;
 }
 
-class MainWindowConnectionError final : public QMainWindow {
+class MainWindow;
+
+class MainWindowConnectionError final : public QDialog {
     Q_OBJECT
 
 public:
     Ui::MainWindowConnectionError *ui;
 
-    MainWindowConnectionError();
+    MainWindowConnectionError(MainWindow *main_window_arg);
     ~MainWindowConnectionError();
 
 private:
     void on_retry_button();
     void open_connection_options();
+
+    MainWindow *main_window;
 };
 
 #endif /* MAIN_WINDOW_PRE_H */

src/admc/main_window_connection_error.ui
@@ -1,93 +1,91 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
  <class>MainWindowConnectionError</class>
- <widget class="QMainWindow" name="MainWindowConnectionError">
+ <widget class="QDialog" name="MainWindowConnectionError">
   <property name="geometry">
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>400</width>
-    <height>120</height>
+    <width>405</width>
+    <height>102</height>
    </rect>
   </property>
   <property name="windowTitle">
-   <string>Connection Error</string>
+   <string>Connection error</string>
   </property>
-  <widget class="QWidget" name="centralwidget">
-   <layout class="QVBoxLayout" name="verticalLayout">
-    <item>
-     <spacer name="verticalSpacer_2">
-      <property name="orientation">
-       <enum>Qt::Vertical</enum>
-      </property>
-      <property name="sizeHint" stdset="0">
-       <size>
-        <width>20</width>
-        <height>40</height>
-       </size>
-      </property>
-     </spacer>
-    </item>
-    <item>
-     <widget class="QLabel" name="label">
-      <property name="text">
-       <string>Failed to connect to domain.</string>
-      </property>
-     </widget>
-    </item>
-    <item>
-     <spacer name="verticalSpacer">
-      <property name="orientation">
-       <enum>Qt::Vertical</enum>
-      </property>
-      <property name="sizeHint" stdset="0">
-       <size>
-        <width>20</width>
-        <height>40</height>
-       </size>
-      </property>
-     </spacer>
-    </item>
-    <item>
-     <layout class="QHBoxLayout" name="horizontalLayout">
-      <item>
-       <spacer name="horizontalSpacer">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>40</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item>
-       <widget class="QPushButton" name="retry_button">
-        <property name="text">
-         <string>Retry</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QPushButton" name="options_button">
-        <property name="text">
-         <string>Connection Options</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QPushButton" name="quit_button">
-        <property name="text">
-         <string>Quit</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </item>
-   </layout>
-  </widget>
+  <layout class="QVBoxLayout" name="verticalLayout">
+   <item>
+    <spacer name="verticalSpacer">
+     <property name="orientation">
+      <enum>Qt::Vertical</enum>
+     </property>
+     <property name="sizeHint" stdset="0">
+      <size>
+       <width>20</width>
+       <height>4</height>
+      </size>
+     </property>
+    </spacer>
+   </item>
+   <item>
+    <widget class="QLabel" name="label">
+     <property name="text">
+      <string>Failed to connect to domain.</string>
+     </property>
+    </widget>
+   </item>
+   <item>
+    <spacer name="verticalSpacer_2">
+     <property name="orientation">
+      <enum>Qt::Vertical</enum>
+     </property>
+     <property name="sizeHint" stdset="0">
+      <size>
+       <width>20</width>
+       <height>4</height>
+      </size>
+     </property>
+    </spacer>
+   </item>
+   <item>
+    <layout class="QHBoxLayout" name="horizontalLayout">
+     <item>
+      <spacer name="horizontalSpacer">
+       <property name="orientation">
+        <enum>Qt::Horizontal</enum>
+       </property>
+       <property name="sizeHint" stdset="0">
+        <size>
+         <width>40</width>
+         <height>20</height>
+        </size>
+       </property>
+      </spacer>
+     </item>
+     <item>
+      <widget class="QPushButton" name="retry_button">
+       <property name="text">
+        <string>Retry</string>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <widget class="QPushButton" name="options_button">
+       <property name="text">
+        <string>Connection Options</string>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <widget class="QPushButton" name="quit_button">
+       <property name="text">
+        <string>Quit</string>
+       </property>
+      </widget>
+     </item>
+    </layout>
+   </item>
+  </layout>
  </widget>
  <resources/>
  <connections/>

commit 45d7abe142e9de0ca9ffe584cbe5802e7bda863c

fix(main): Fix a connection error
This patch fixes the problem that was introduced in
6efd30096f69f3b94a95cf6ef9e98dec7607f3b1: When the setting "show login window on
startup" is enabled, ADMC would always show a "Connection error" window instead.
This patch fixes such behavior.

See <https://bugzilla.altlinux.org/show_bug.cgi?id=59659>

* src/admc/main.cpp (main): Check if "SETTING_show_login_window_on_startup"
option is enabled; properly show connection window on start instead of
connection error.

src/admc/main.cpp
@@ -4,6 +4,7 @@
  *
  * Copyright (C) 2020-2025 BaseALT Ltd.
  * Copyright (C) 2020-2025 Dmitry Degtyarev
+ * Copyright (C) 2026 Artyom V. Poptsov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -138,7 +139,8 @@ int main(int argc, char **argv) {
             main_window->show_changelog_on_update();
         }
         else {
-            if (krb5_client->active_tgt_principals().isEmpty()) {
+            bool no_principals = krb5_client->active_tgt_principals().isEmpty();
+            if (show_login_window || no_principals) {
                 main_window->open_auth_dialog();
             } else {
                 error_window = new MainWindowConnectionError(main_window);