e52aea4eb83ed5bd7f73134f8c3335869d4539a3
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-07-21 16:46:29 +0300

    style(fsmo_utils): Improve the coding style
    * src/admc/fsmo/fsmo_utils.cpp: Improve the coding style.

--- src/admc/fsmo/fsmo_utils.cpp
+++ src/admc/fsmo/fsmo_utils.cpp
@@ -1,8 +1,9 @@
 /*
  * ADMC - AD Management Center
  *
- * Copyright (C) 2023-2025 BaseALT Ltd.
+ * Copyright (C) 2023-2026 BaseALT Ltd.
  * Copyright (C) 2023-2025 Semyon Knyazev
+ * Copyright (C) 2026 Artyom V. Poptsov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,15 +39,25 @@ QString dn_from_role(FSMORole role)
     const QString domain_dn = g_adconfig->domain_dn();
 
     switch (role) {
-        case FSMORole_DomainDNS: return QString("CN=Infrastructure,DC=DomainDnsZones,%1").arg(domain_dn);
-        case FSMORole_ForestDNS: return QString("CN=Infrastructure,DC=ForestDnsZones,%1").arg(domain_dn);
-        case FSMORole_PDCEmulation: return domain_dn;
-        case FSMORole_Schema: return g_adconfig->schema_dn();
-        case FSMORole_DomainNaming: return g_adconfig->partitions_dn();
-        case FSMORole_Infrastructure: return QString("CN=Infrastructure,%1").arg(domain_dn);
-        case FSMORole_RidAllocation: return QString("CN=RID Manager$,CN=System,%1").arg(domain_dn);
+        case FSMORole_DomainDNS:
+            return QString("CN=Infrastructure,DC=DomainDnsZones,%1")
+                .arg(domain_dn);
+        case FSMORole_ForestDNS:
+            return QString("CN=Infrastructure,DC=ForestDnsZones,%1")
+                .arg(domain_dn);
+        case FSMORole_PDCEmulation:
+            return domain_dn;
+        case FSMORole_Schema:
+            return g_adconfig->schema_dn();
+        case FSMORole_DomainNaming:
+            return g_adconfig->partitions_dn();
+        case FSMORole_Infrastructure:
+            return QString("CN=Infrastructure,%1").arg(domain_dn);
+        case FSMORole_RidAllocation:
+            return QString("CN=RID Manager$,CN=System,%1").arg(domain_dn);
 
-        case FSMORole_COUNT: break;
+        case FSMORole_COUNT:
+            break;
     };
 
     return QString();
@@ -55,10 +66,12 @@ QString dn_from_role(FSMORole role)
 QString current_master_for_role_dn(AdInterface &ad, QString role_dn)
 {
     const AdObject role_object = ad.search_object(role_dn);
-    const QString master_settings_dn = role_object.get_string(ATTRIBUTE_FSMO_ROLE_OWNER);
+    const QString master_settings_dn =
+        role_object.get_string(ATTRIBUTE_FSMO_ROLE_OWNER);
     const QString master_dn = dn_get_parent(master_settings_dn);
     const AdObject master_object = ad.search_object(master_dn);
-    const QString current_master = master_object.get_string(ATTRIBUTE_DNS_HOST_NAME);
+    const QString current_master =
+        master_object.get_string(ATTRIBUTE_DNS_HOST_NAME);
     return current_master;
 }
 
@@ -82,7 +95,8 @@ void connect_to_PDC_emulator(AdInterface &ad, ConsoleWidget *console)
 {
     connect_host_with_role(ad, FSMORole_PDCEmulation);
     console->refresh_scope(console->domain_info_index());
-    g_status->add_message(QObject::tr("PDC-Emulator is connected"), StatusType_Success);
+    g_status->add_message(QObject::tr("PDC-Emulator is connected"),
+                          StatusType_Success);
 }
 
 QString current_master_for_role(AdInterface &ad, FSMORole role)
@@ -94,15 +108,23 @@ QString current_master_for_role(AdInterface &ad, FSMORole role)
 QString string_fsmo_role(FSMORole role)
 {
     switch (role) {
-        case FSMORole_DomainDNS: return "Domain DNS";
-        case FSMORole_ForestDNS: return "Forest DNS";
-        case FSMORole_PDCEmulation: return "PDC Emulator";
-        case FSMORole_Schema: return "Schema master";
-        case FSMORole_DomainNaming: return "Domain naming master";
-        case FSMORole_Infrastructure: return "Infrastructure master";
-        case FSMORole_RidAllocation: return "RID master";
+        case FSMORole_DomainDNS:
+            return "Domain DNS";
+        case FSMORole_ForestDNS:
+            return "Forest DNS";
+        case FSMORole_PDCEmulation:
+            return "PDC Emulator";
+        case FSMORole_Schema:
+            return "Schema master";
+        case FSMORole_DomainNaming:
+            return "Domain naming master";
+        case FSMORole_Infrastructure:
+            return "Infrastructure master";
+        case FSMORole_RidAllocation:
+            return "RID master";
 
-        case FSMORole_COUNT: break;
+        case FSMORole_COUNT:
+            break;
     };
 
     return QString();
