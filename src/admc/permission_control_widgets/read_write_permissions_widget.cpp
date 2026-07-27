6a0fb0d7b5183d4aeda0985606ea0c2d1a1ef1d1
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-07-27 15:20:14 +0300

    style(read_write_permissions_widget): Add copyright header
    * src/admc/permission_control_widgets/read_write_permissions_widget.cpp,
    src/admc/permission_control_widgets/read_write_permissions_widget.h: Add the
    missing copyright header.

--- src/admc/permission_control_widgets/read_write_permissions_widget.cpp
+++ src/admc/permission_control_widgets/read_write_permissions_widget.cpp
@@ -1,3 +1,25 @@
+/*
+ * ADMC - AD Management Center
+ *
+ * Copyright (C) 2024-2026 BaseALT Ltd.
+ * Copyright (C) 2024 Semyon Knyazev
+ * Copyright (C) 2024 Gleb Popov
+ * Copyright (C) 2026 Artyom V. Poptsov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "read_write_permissions_widget.h"
 
 #include "adldap.h"
